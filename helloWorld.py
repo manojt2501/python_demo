5922a2c8badd3489a96e96e228c7d594e24fd636
manojt2501 <manojt2501@gmail.com>
2021-11-03 18:08:35 +0530

    time delay with timestamp

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,8 @@
 import time
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
 print('hello Manoj! wow great it works')
+print(current_time)
 time.sleep(10)
+print(current_time)
 print('after 10 seconds')
